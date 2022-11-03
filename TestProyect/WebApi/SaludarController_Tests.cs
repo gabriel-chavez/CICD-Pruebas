35f39d4806c07b1704937bfc63b20cf0c1fbb128
Gabriel Chavez <gabriel.chavez.r@gmail.com>
2022-11-03 18:55:57 -0400

    actualización

---
+++ TestProyect/WebApi/SaludarController_Tests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApiSaludar.Controllers;
+
+namespace TestProyect.WebApi
+{
+    public class SaludarController_Tests
+    {
+        [Fact]
+        public void Get_Correctly()
+        {
+            var resultadoEsperado = "Hola Mundo!";
+            SaludarController saludarController = new SaludarController();
+            var resultado = saludarController.Get();
+            Assert.Equal(resultadoEsperado, resultado);
+        }
+    }
+}
---
+++ WebApiSaludar/Controllers/SaludarController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiSaludar.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SaludarController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Hola Mundo!!";
+        }
+
+    }
+}
