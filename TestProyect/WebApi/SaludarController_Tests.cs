85c013e4754a66f4b05a2fe46312b5edb4bcf0b4
Gabriel Chavez <gabriel.chavez.r@gmail.com>
2022-11-10 01:51:31 -0400

    prueba

--- TestProyect/WebApi/SaludarController_Tests.cs
+++ TestProyect/WebApi/SaludarController_Tests.cs
@@ -12,7 +12,7 @@ namespace TestProyect.WebApi
         [Fact]
         public void Get_Correctly()
         {
-            var resultadoEsperado = "Hola Geovanna!";
+            var resultadoEsperado = "Hola Geovanna1!";
             SaludarController saludarController = new SaludarController();
             var resultado = saludarController.Get();
             Assert.Equal(resultadoEsperado, resultado);
